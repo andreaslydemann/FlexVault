41bb7cd66c37334d6eda55b8c737266457ab50bd
Your Name <you@example.com>
2016-05-09 17:03:05 +0200

    added routing to script

--- FV_SPI/mk_psoc.sh
+++ FV_SPI/mk_psoc.sh
@@ -1,5 +1,7 @@
 rmmod spi.ko
 rmmod hotplug_psoc_spi_device.ko
+echo 0x3 > /sys/class/cplddrv/cpld/spi_route_reg	
+echo 0x1 > /sys/class/cplddrv/cpld/ext_serial_if_route_reg
 insmod spi.ko
 insmod hotplug_psoc_spi_device.ko
 rm /dev/spidev0
